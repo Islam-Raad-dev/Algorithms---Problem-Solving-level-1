9906bbae2ae09405e3dd20815c0682f40aff9d5c
Islam Raad <islamraad.dev@gmail.com>
2026-03-14 14:56:27 +0300

    Update Problem-12.cpp

--- Algorithms & Problem Solving level 1/Problem-12.cpp
+++ Algorithms & Problem Solving level 1/Problem-12.cpp
@@ -6,7 +6,7 @@ using namespace std;
 void ReadNumbers(int& Num1, int& Num2)
 {
   
-    cout << "Please enter Number 1 ? " << endl;
+    cout << "Please enter Number 1: " << endl;
     cin >> Num1;
 
 
